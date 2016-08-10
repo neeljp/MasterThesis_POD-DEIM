8e80c29a6b1beec3800e4c9b12e386cc13d66ff6
Neel Peters <njp@informatik.uni-kiel.de>
2016-08-10 16:08:20 +0200

    change folder to /space/hydra/njp/

--- POD_mat.py
+++ POD_mat.py
@@ -1,11 +1,16 @@
 import numpy as np
 import util
 import petsc_io as io
-pod_vecs = util.constructSnapshotMatrix("/home/njp/metos3d/work/simulation/POD_DEIM/3/","sp%.4dts%.4dN.petsc"
+pod_vecs = util.constructSnapshotMatrix("/space/hydra/njp/work/0/","sp%.4dts%.4dN.petsc"
+                                    ,3,12,12)
+for i in range(1,10):
+        pod_vecs = np.column_stack((pod_vecs,util.constructSnapshotMatrix("/space/hydra/njp/work/%.d/" % i,"sp%.4dts%.4dN.petsc"
+                                    ,3,12,12)))
+
+
+#pod_vecs_2 = util.constructSnapshotMatrix("/home/njp/metos3d/work/simulation/POD_DEIM/4/","sp%.4dts%.4dN.petsc"
                                     ,3000,12,12)
-pod_vecs_2 = util.constructSnapshotMatrix("/home/njp/metos3d/work/simulation/POD_DEIM/4/","sp%.4dts%.4dN.petsc"
-                                    ,3000,12,12)
-pod_vecs_3 = np.column_stack((pod_vecs,pod_vecs_2))
-print(pod_vecs_3.shape)
+#pod_vecs_3 = np.column_stack((pod_vecs,pod_vecs_2))
+print(pod_vecs.shape)
 #p = csr_matrix(pod_vecs)
-io.write_PETSc_mat_dense(pod_vecs_3,"POD_mat.petsc")
+io.write_PETSc_mat_dense(pod_vecs,"POD_mat.petsc")
